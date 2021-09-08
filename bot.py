b67a04fb351cb2039313fdc7e8ada862cd8f8374
ᴸᵒᵏⁱ <72887386+Oxidisedman@users.noreply.github.com>
2021-09-08 19:57:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -203,7 +203,7 @@ async def ytmusic(client, message: Message):
     query = ''
 
     pablo = await client.send_message(
-        message.chat.id, f"`Getting {urlissed} From Youtube Servers. Please Wait.`"
+        message.chat.id, f"`Getting {''} From Youtube Servers. Please Wait.`"
     )
     if not urlissed:
         await pablo.edit("Invalid Command Syntax, Please Check Help Menu To Know More!")
@@ -254,7 +254,7 @@ async def ytmusic(client, message: Message):
         progress_args=(
             pablo,
             c_time,
-            f"`Uploading {urlissed} Song From YouTube Music!`",
+            f"`Uploading {''} Song From YouTube Music!`",
             file_stark,
         ),
     )
