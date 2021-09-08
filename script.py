67fc01215bdbc554dbad24b7a123b0629b0f19fb
ᴸᵒᵏⁱ <72887386+Oxidisedman@users.noreply.github.com>
2021-09-08 09:55:05 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+class Script(object):
+
+    HELP_SCRIPT = """ "**hello** {}, **I'm Arina. I'm a music bot written in** **__python__** **using** **__pyrogram__**.
+**Major commads**
+/start -> For start the bot.
+/help -> To get help string.
+/about -> To get about string.
+/source -> To get bot source code. 
+/song  -> To get song.
+[Example -> `/song arora runway`]
+"""
+
+    CREATOR = """ hoi {} i'm Arina. I'm a bot written in python (**python-3**, __python telegram bot library__) using pyrogram 🔥\n
+I'm created by **__@Oxidisedman__**\n
+I'm a open source bot. My Souce code is avalible for you.. 
+"""
