37cdc557e43a99e5484d00035e30878dfc941068
ᴸᵒᵏⁱ <72887386+Oxidisedman@users.noreply.github.com>
2021-09-08 09:52:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+class Config:
+
+    BOT_TOKEN = "" # 👻 get it from @botfather (telegram) by sending /newbot command.
+    API_ID = ""  # 👻 Same as APP_ID. Get it from my.telegram.org.
+    API_HASH = "" # 👻 Get it from my.telegram.org.
